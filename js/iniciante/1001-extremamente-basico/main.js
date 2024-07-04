771f857adce989a2c34f6efb0a2c636c44b5e718
lsrdot <lucas.siqueiraribeiro99@gmail.com>
2024-07-04 20:40:58 -0300

    daily update

---
+++ js/iniciante/1001-extremamente-basico/main.js
@@ -0,0 +1,13 @@
+const readline = require('node:readline')
+
+const rl = readline.createInterface({
+	input: readline.stdin,
+	output: process.stdout,
+});
+
+rl.question(x => {
+	rl.close;
+});
+
+
+
