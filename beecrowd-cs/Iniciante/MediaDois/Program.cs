2278c3ab5aa538865bbd63e306f2b286d64f3b58
lsrdot <lucas.siqueiraribeiro99@gmail.com>
2024-09-15 17:10:55 -0300

    adding py 1001 problem sol.

---
+++ beecrowd-cs/Iniciante/MediaDois/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MediaUm
+{
+    class URI
+    {
+        int PESO_A, PESO_B, PESO_C;
+
+        public static void Main()
+        {            
+            double notaA, notaB, notaC;
+
+            var aux = Console.ReadLine();
+            !(string.IsNullOrWhiteSpace(aux)) ? notaA = double.Parse(aux) : notaA = .0;
+
+        }
+    }
+}
