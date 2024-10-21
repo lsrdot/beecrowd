91c2d21a48e6b99705f5340be95c4c7d4664cb99
lsrdot <lucas.siqueiraribeiro99@gmail.com>
2024-10-21 20:52:43 -0300

    adding more solutions

---
+++ beecrowd-cs/Iniciante/Diferenca/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Diferenca;
+
+class URI
+{
+    static void Main()
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        int d = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"DIFERENCA = {a * b - c * d}");
+    }
+}
--- beecrowd-cs/Iniciante/MediaDois/Program.cs
+++ beecrowd-cs/Iniciante/MediaDois/Program.cs
@@ -1,16 +1,31 @@
-﻿namespace MediaUm
+﻿namespace MediaDois;
+
+class URI
 {
-    class URI
+    private static readonly UInt16 PesoA = 2;
+    private static readonly UInt16 PesoB = 3;
+    private static readonly UInt16 PesoC = 5;
+
+    static void Main()
     {
-        int PESO_A, PESO_B, PESO_C;
+        // double notaA = 0, notaB = 0, notaC = 0;
+        // double? nota = double.TryParse(Console.ReadLine(), out var parsingA) ? parsingA : null;
+        // if (nota.HasValue)
+        //     notaA = nota.Value;
+        // nota = double.TryParse(Console.ReadLine(), out var parsingB) ? parsingB : null;
+        // if (nota.HasValue)
+        //     notaB = nota.Value;
+        // nota = double.TryParse(Console.ReadLine(), out var parsingC) ? parsingC : null;
+        // if (nota.HasValue)
+        //     notaC = nota.Value;
+        // var media = (notaA * PesoA + notaB * PesoB + notaC * PesoC) / (PesoA + PesoB + PesoC);
+        // Console.WriteLine($"MEDIA = {media:F2}");
 
-        public static void Main()
-        {            
-            double notaA, notaB, notaC;
-
-            var aux = Console.ReadLine();
-            !(string.IsNullOrWhiteSpace(aux)) ? notaA = double.Parse(aux) : notaA = .0;
-
-        }
+        double notaA = Convert.ToDouble(Console.ReadLine());
+        double notaB = Convert.ToDouble(Console.ReadLine());
+        double notaC= Convert.ToDouble(Console.ReadLine());
+        var media = (notaA * PesoA + notaB * PesoB + notaC * PesoC) / (PesoA + PesoB + PesoC);
+        Console.WriteLine($"MEDIA = {media:F1}");
+        
     }
 }
