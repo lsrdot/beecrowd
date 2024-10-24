cd45e7571c3e6df5f2064a670d3d44afb619ecc7
lsrdot <lucas.siqueiraribeiro99@gmail.com>
2024-10-24 19:32:24 -0300

    adding sol.

--- beecrowd-cs/Iniciante/Salario/Program.cs
+++ beecrowd-cs/Iniciante/Salario/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace salario;
+
+class URI
+{
+    static void Main()
+    {
+        var number = int.TryParse(Console.ReadLine(), out int x) ? x : -1;
+        var hoursWorked = int.TryParse(Console.ReadLine(), out int y) ? y : -1;
+        var salary = double.TryParse(Console.ReadLine(), out double s) ? s : -1;
+        Console.WriteLine($"NUMBER = {number}" + "\n" + $"SALARY = U$ {hoursWorked * salary:F2}");
+    }
+}
---
+++ beecrowd-cs/Iniciante/SalarioComBonus/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SalarioComBonus;
+
+class Program
+{
+    static void Main()
+    {
+        string temp = Console.ReadLine();
+        string name;
+        if (!string.IsNullOrEmpty(temp))
+            name = temp;
+        double salary = double.TryParse(Console.ReadLine(), out double x) ? x : 0;
+        double sales = double.TryParse(Console.ReadLine(), out double y) ? y : 0;
+        Console.WriteLine($"TOTAL = R$ {salary + (sales * .15):F2}");
+    }
+}
